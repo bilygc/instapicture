a6d53a299089b9d784156d2db09bfbb2363d72e0
bilygc <licbgomez@gmail.com>
2021-10-19 13:58:08 -0600

    serviceworker register fix

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import './index.css'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import reducers from './reducers'
 
